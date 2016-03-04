84146eb83e35a54172989272ea687384ce730215
M-Younas <younus.ubit@outlook.com>
2016-03-03 23:06:48 -0800

    code added

--- linked_llist.cpp
+++ linked_llist.cpp
@@ -1,12 +1,35 @@
 #include<stdio.h>
+#include<stdlib.h>
 struct Node{
 	int data;
 	Node *link;
 };
-void append(int a,int num){
+
+void append(Node *&head,int num){
+	Node *temp;
 	
+	temp=(Node*)malloc(sizeof(Node));
+	temp->data=num;
+	temp->link=NULL;
+	if(head==NULL){
+		head=temp;
+	}
+	else{
+		Node *th;
+		th=head;
+		while(th->link!=NULL){
+			th=th->link;
+			th->link=temp;
+			printf("pehla=%d next %u",th->data,th->link);
+		}
+	}
 }
 main(){
-	Node n,*pn;
-	
+	struct Node *list1=NULL;
+	append(list1,1);
+	//printf("pehla=%d next %u",list1->data,list1->link);
+	append(list1,3);
+	//printf("\npehla=%d next %u",list1->data,list1->link);
 }
+
+
