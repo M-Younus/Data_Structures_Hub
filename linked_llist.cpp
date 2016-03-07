aa0097af1a8ccdce8512654f1f11fa4e592b1289
M-Younas <younus.ubit@outlook.com>
2016-03-07 10:19:21 -0800

    without func done

--- linked_llist.cpp
+++ linked_llist.cpp
@@ -5,18 +5,18 @@ struct Node{
 	Node *link;
 };
 
-void append(Node *&head,int num){
+void append(Node **head,int num){
 	Node *temp;
 	
 	temp=(Node*)malloc(sizeof(Node));
 	temp->data=num;
 	temp->link=NULL;
 	if(head==NULL){
-		head=temp;
+		*head=temp;
 	}
 	else{
 		Node *th;
-		th=head;
+		th=*head;
 		while(th->link!=NULL){
 			th=th->link;
 			th->link=temp;
@@ -26,10 +26,63 @@ void append(Node *&head,int num){
 }
 main(){
 	struct Node *list1=NULL;
-	append(list1,1);
+	append(&list1,1);
 	//printf("pehla=%d next %u",list1->data,list1->link);
-	append(list1,3);
+	append(&list1,3);
 	//printf("\npehla=%d next %u",list1->data,list1->link);
 }
+
+//without functions
+//#include<stdlib.h>
+//#include<stdio.h>
+//#include<conio.h>
+//struct node{
+//	int x;
+//	struct node *next;
+//};
+//main(){
+//	struct node *root=NULL;
+//	struct node *conductor=NULL;
+//	
+//	char choice;int num;
+//	printf("want to enter value:");
+////	scanf("%c",&choice);
+//	choice=getche();puts("");
+//	if(choice=='y')
+//{
+//	root=(struct node*)malloc(sizeof(struct node));
+//	printf("Enter num:");
+//	scanf("%d",&num);
+//	root->x=num;
+//	root->next=0;
+//	conductor=root;	
+//	
+//	while(1==1){
+//		printf("more:");
+//		choice=getche();puts("");
+////		scanf("%c",&choice);
+//		if(choice!='y')
+//		break;
+//		else{
+//		conductor->next=(struct node*)malloc(sizeof(struct node));
+//		conductor=conductor->next;
+//		printf("Enter num:");
+//		scanf("%d",&num);
+//		conductor->x=num;
+//		conductor->next=0;
+//		}
+//	}
+//}
+//	conductor=root;
+//	if(conductor!=NULL){
+//		while(conductor!=NULL){
+//		printf("%d\n",conductor->x);
+//		conductor=conductor->next;
+//		}	
+//	}
+//	else{
+//		puts("Linked list in empty");	
+//	}
+//}
 
 
