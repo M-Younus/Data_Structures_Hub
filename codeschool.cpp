e11c2334af16650ba683176276dd5db152831f3a
M-Younas <younus.ubit@outlook.com>
2016-03-24 04:26:39 +0500

    insert return head

--- codeschool.cpp
+++ codeschool.cpp
@@ -4,32 +4,33 @@ struct Node{
 	int data;
 	struct Node* next;	
 };
-struct Node* head;
-void Insert(int x){
+//struct Node* head;
+Node* Insert(Node* head,int x){
 	Node* temp=(Node*)malloc(sizeof(struct Node));
 	temp->data=x;
 	temp->next=head;
-	head=temp;
+	return head=temp;
+	
 }
-void Print(){
-	Node* temp=head;
+void Print(Node* head){
+//	Node* temp=head;
 	printf("List is: ");
-	while(temp!=NULL){
-		printf(" %d",temp->data);
-		temp=temp->next;
+	while(head!=NULL){
+		printf(" %d",head->data);
+		head=head->next ;
 	}
 	printf("\n");
 }
 int main(){
-	head=NULL;
+	Node* head=NULL;
 	printf("How many numbers?\n");
 	int n,i,x;
 	scanf("%d",&n);
 	for(i=0;i<n;i++){
 		printf("Enter the num\n");
 		scanf("%d",&x);
-		Insert(x);
-		Print();
+		head=Insert(head,x);
+		Print(head);
 	}
 }
 
