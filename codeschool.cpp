2c9c341ade25af479ddc98b5161716918f72fab2
M-Younas <younus.ubit@outlook.com>
2016-03-24 10:57:05 +0500

    add at begining

---
+++ codeschool.cpp
@@ -0,0 +1,35 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct Node{
+	int data;
+	struct Node* next;	
+};
+struct Node* head;
+void Insert(int x){
+	Node* temp=(Node*)malloc(sizeof(struct Node));
+	temp->data=x;
+	temp->next=head;
+	head=temp;
+}
+void Print(){
+	Node* temp=head;
+	printf("List is: ");
+	while(temp!=NULL){
+		printf(" %d",temp->data);
+		temp=temp->next;
+	}
+	printf("\n");
+}
+int main(){
+	head=NULL;
+	printf("How many numbers?\n");
+	int n,i,x;
+	scanf("%d",&n);
+	for(i=0;i<n;i++){
+		printf("Enter the num\n");
+		scanf("%d",&x);
+		Insert(x);
+		Print();
+	}
+}
+
