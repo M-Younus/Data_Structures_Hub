0b5b8bc510bfd452340229db795046110c42a85c
M-Younas <younus.ubit@outlook.com>
2016-04-30 07:17:27 +0500

    bht prac

--- BHT.cpp
+++ BHT.cpp
@@ -2,11 +2,58 @@
 #include<conio.h>
 #include<stdlib.h>
 
-//min BHP
+int min(int a,int b){
+	if(a<b)
+	return a;
+	else
+	return b;
+}
+
+void swap(int *a,int *b){
+	int temp;
+	temp=*a;
+	*a=*b;
+	*b=temp;
+}
+
+void Delete(int ar[],int size){
+	puts("\nDelete");
+	int i=1;int max=size;int left,right;
+
+	while(i<=max&&max!=0){
+		printf("%d\n",ar[i]);
+		ar[i]=ar[max];
+		left=ar[2*i];
+		right=ar[2*i+1];
+		if(ar[i]>left){
+			int temp;
+			temp=ar[i];
+			ar[i]=left;
+			ar[i*2]=temp;
+		}
+		else if(ar[i]>right){
+			int temp;
+			temp=ar[i];
+			ar[i]=right;
+			ar[i*2+1]=temp;
+		}
+		max--;
+	}
+
+}
+
 main(){
-	int arr[]={0,5,10,17,-5,0,13,10,22,55,79,-10,15};int i,j,k,x,y,z;
-	int bhp[12];
-	for(j=1;j<=12;j++){
+	
+//	int a=3;
+//	int b=5;
+//	swap(&a,&b);
+//	printf("a=%d,,b=%d",a,b);
+//	
+//	exit(0);
+	
+	int arr[]={0,5,10,17,-5,13,10,11};int i,j,k,x,y,z;
+	int bhp[8];
+	for(j=1;j<=7;j++){
 		i=j;
 		if(i==1){
 			bhp[i]=arr[i];
@@ -17,7 +64,7 @@ main(){
 			x=bhp[i];
 			y=bhp[i/2];
 			
-			while(x>y&&i!=1){
+			while(x<y&&i!=1){
 				
 				bhp[i/2]=x;
 				bhp[i]=y;
@@ -32,6 +79,8 @@ main(){
 		}
 	}
 	printf("\n\n");
-	for(i=0;i<=12;i++)
+	for(i=1;i<=7;i++)
 	printf("\n%d",bhp[i]);
+	
+	Delete(bhp,7);
 }
---
+++ row measure.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include <stdio.h>
+int main(){
+    int dim,N=1,PoS,ReqAdd=0,i,j;
+    printf("enter no. of dimensions:\n");
+    scanf("%d",&dim);
+    printf("enter size of each dimension\n");
+    int *S=malloc(sizeof(int));
+    int *I=malloc(sizeof(int));
+    for(i=0;i<dim;i++){
+        scanf("%d",&S[i]);
+        N=N*S[i];
+    }
+    printf("total no. of elements=%d\n",N);
+    int *data=malloc(sizeof(int));
+    printf("enter all elements\n");
+    for(i=0;i<N;i++)
+        scanf("%d",&data[i]);
+    printf("enter index of required value\n");
+    for(i=0;i<dim;i++){
+        scanf("%d",&I[i]);
+        if(I[i]>=S[i]){
+        	printf("invalid index. enter index again\n");
+        	i=-1;
+		}
+	}
+    for(i=0;i<dim;i++){
+        PoS=I[i];
+        for(j=i+1;j<dim;j++){
+            PoS=PoS*S[j];
+        }
+        ReqAdd=ReqAdd+PoS;
+    }
+    printf("value at given address=%d",data[ReqAdd]);
+}
